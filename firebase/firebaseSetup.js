d5b032cb0c94d19da615f53aec071c3b8d41193f
ZacharyVoceLTU <20731993@students.latrobe.edu.au>
2025-05-12 01:22:49 +1000

    Firebase Auth fix

--- firebase/firebaseSetup.js
+++ firebase/firebaseSetup.js
@@ -1,17 +1,16 @@
 // Firebase Imports
 import { initializeApp } from "firebase/app";
-// import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { firebaseConfig } from './firebase';
 
-// ERROR: No work
-// const auth = getAuth(app);
-
-
 let app = initializeApp(firebaseConfig)
+
+// ERROR: No work
+const auth = getAuth(app);
 
 const db = getFirestore(app);
 
-export { db };
+export { auth, db };
 
